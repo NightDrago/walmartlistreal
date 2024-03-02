07ec0cfd4b3350d4cdc6157b66c0651ea5ea4d3f
sphericle <86196223+sphericle@users.noreply.github.com>
2024-03-02 10:19:51 -0500

    i know javascript

--- js/components/List/LevelAuthors.js
+++ js/components/List/LevelAuthors.js
@@ -24,7 +24,7 @@ export default {
             <template v-else-if="creators.length === 1">
                 <div class="type-title-sm">Creator</div>
                 <p class="type-body">
-                    <span>{{ author }}</span>
+                    <span>{{ creator }}</span>
                 </p>
                 <div class="type-title-sm">Verifier</div>
                 <p class="type-body">

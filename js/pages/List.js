d582173a39ff10d08d9cc661f63336fdc716b962
sphericle <86196223+sphericle@users.noreply.github.com>
2024-03-04 22:53:55 -0500

    pls

--- js/pages/List.js
+++ js/pages/List.js
@@ -58,7 +58,7 @@ export default {
                     <ul class="stats">
                         <li>
                             <div class="type-title-sm">Song</div>
-                            <p>{{ level.song-name }}</p>
+                            <p>{{ level.song-title }}</p>
                         </li>
                     </ul>
                     <h2>Records</h2>

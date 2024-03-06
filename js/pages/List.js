94f079ee8b5f4318931e16f8537235e8613aa82f
sphericle <86196223+sphericle@users.noreply.github.com>
2024-03-05 19:37:52 -0500

    pray

--- js/pages/List.js
+++ js/pages/List.js
@@ -58,7 +58,8 @@ export default {
                     <ul class="stats">
                         <li>
                             <div class="type-title-sm">Song</div>
-                            <p><a href={{ level.song-link }} target="_blank"><u>{{ level.song-title }}</u></a></p>s
+                            <p><a :href="level['song-link']" target="_blank"><u>{{ level['song-title'] }}</u></a></p>
+
                         </li>
                     </ul>
                     <h2>Records</h2>

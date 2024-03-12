0b33740fa1087cd7890d8742bc518cfe4d7a6c9d
sphericle <86196223+sphericle@users.noreply.github.com>
2024-03-11 20:43:26 -0400

    YOOO

--- js/pages/List.js
+++ js/pages/List.js
@@ -14,6 +14,21 @@ const roleIconMap = {
     trial: "user-lock",
 };
 
+function copyID() {
+    // Get the text field
+    var copyText = document.getElementById("myInput");
+  
+    // Select the text field
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); // For mobile devices
+  
+     // Copy the text inside the text field
+    navigator.clipboard.writeText(copyText.value);
+  
+    // Alert the copied text
+    alert("Copied ID!");
+  }
+
 export default {
     components: { Spinner, LevelAuthors },
     template: `

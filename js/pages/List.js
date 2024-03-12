6bc5b35de19d9dd759d16e9e3c99d8616720ba9a
sphericle <86196223+sphericle@users.noreply.github.com>
2024-03-11 21:03:00 -0400

    we love vue in this house

--- js/pages/List.js
+++ js/pages/List.js
@@ -69,7 +69,7 @@ export default {
                         </li>
                         <li>
                             <div class="type-title-sm">ID</div>
-                            <button @click="copyID()">{{ level.id }}</button>
+                            <button @click="copyID">Copy ID</button>
                         </li>
                         <li>
                             <div class="type-title-sm">Password</div>

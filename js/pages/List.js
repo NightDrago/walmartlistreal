d3aa829e5d11ffb2e9668fcead403682fb8132f4
sphericle <86196223+sphericle@users.noreply.github.com>
2024-03-16 15:27:20 -0400

    gtg lol

--- js/pages/List.js
+++ js/pages/List.js
@@ -37,7 +37,7 @@ export default {
                 </table>
             </div>
             <div class="level-container">
-                <div class="level" v-if="level">
+                <div class="level" v-if="level" && "level.id!=0>
                     <h1>{{ level.name }}</h1>
                     <LevelAuthors :author="level.author" :creators="level.creators" :verifier="level.verifier" :song-title="level.song-title" :song-link="level.song-link"></LevelAuthors>
                     <iframe class="video" id="videoframe" :src="video" frameborder="0"></iframe>
@@ -85,6 +85,9 @@ export default {
                         </tr>
                     </table>
                 </div>
+                <div v-else-if="level.id==0" class="level" style="height: 100%; justify-content: center; align-items: center;">
+                    <h1>{{ level.name }}</h1>
+                    <p>The levels below are {{ level.name.replace("(", "").replace(")", "") }}.</p>
                 <div v-else class="level" style="height: 100%; justify-content: center; align-items: center;">
                     <p>(ノಠ益ಠ)ノ彡┻━┻</p>
                 </div>

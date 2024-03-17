44f1e773aa586be5e43da78e37e279f46c1bd1bd
sphericle <86196223+sphericle@users.noreply.github.com>
2024-03-16 22:44:00 -0400

    removed 1 SPACE!

--- js/pages/List.js
+++ js/pages/List.js
@@ -69,7 +69,7 @@ export default {
                     <p v-else-if="selected +1 <= 150"><strong>100%</strong> or better to qualify</p>
                     <p v-else>This level does not accept new records.</p>
                     <table class="records">
-                        <tr v-for="record in level.records" v-if="record.user !=Ultrobro" class="record">
+                        <tr v-for="record in level.records" v-if="record.user!=Ultrobro" class="record">
                             <td class="percent">
                                 <p>{{ record.percent }}%</p>
                             </td>
